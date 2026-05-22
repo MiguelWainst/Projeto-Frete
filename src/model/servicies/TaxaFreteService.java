0db4c4def7be79e903030a09395cf1caa03ec4e4
Stein <miguel.wainst@gmail.com>
2026-05-22 15:20:42 -0300

    Melhorando algumas coisas e polindo outras.

--- src/model/servicies/TaxaFreteService.java
+++ src/model/servicies/TaxaFreteService.java
@@ -22,7 +22,6 @@ public class TaxaFreteService {
 
         // Esta é uma lista de interfaces e tudo o que estende ela.
         List<? extends ITaxaTransporte> list = Arrays.asList(new TAereo(), new TMaritimo(), new TTerrestre());
-        List<Double> precoComTaxa = new ArrayList<>(); // Lista feita para guardar os preços com taxa.
 
         int i=0; // Índicie responsável por controlar a lista.
         for (ITaxaTransporte taxaTransporte:list) {
@@ -30,7 +29,7 @@ public class TaxaFreteService {
             /* Adiciona a taxa de frete conforme a regra de negócio
             da classe que implementa a interface ITaxaTransporte. */
             Double taxa = taxaTransporte.TaxaFrete(peso);
-            precoComTaxa.add(taxa + preco); // Adiciona à lista o preço com a taxa.
+            Double precoComTaxa = taxa + preco; // Adiciona à lista o preço com a taxa.
 
             // Laço que adiciona imposto e adicional de tipo de carga ao preço final.
             Double precoTotal = 0.0; // Guarda o preço final a ser pago.
@@ -51,7 +50,7 @@ public class TaxaFreteService {
                 adicionalCarga = ADICIONAL_INFLAMAVEL;
             }
 
-            precoTotal += precoComTaxa.get(i) + impostoPreco; // Soma o preço com taxa e o imposto ao preço total.
+            precoTotal += precoComTaxa + impostoPreco; // Soma o preço com taxa e o imposto ao preço total.
 
             // Adiciona à lista "resultado" uma nova instância de ResultadoCalculo.
             /* Essa ".add" é responsável por passar um objeto do tipo ResultadoCalculo
